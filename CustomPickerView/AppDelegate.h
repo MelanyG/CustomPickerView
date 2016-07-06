833b82edf9fe50a6a687b8d712f9ab79068d7bb3
Melaniia Hulianovych <mhul@macmini785.local>
2016-07-06 14:44:52 +0300

    Initial Commit

---
+++ CustomPickerView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CustomPickerView
+//
+//  Created by Melaniia Hulianovych on 7/6/16.
+//  Copyright © 2016 Melaniia Hulianovych. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CustomPickerView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CustomPickerView
+//
+//  Created by Melaniia Hulianovych on 7/6/16.
+//  Copyright © 2016 Melaniia Hulianovych. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
